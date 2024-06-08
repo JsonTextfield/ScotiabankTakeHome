897235b1c5c8e1bf7011bd833a5530224d01d1ad
jason <tehblackboy@gmail.com>
2024-06-08 07:41:38 -0400

    created the search bar

--- app/src/main/java/com/jsontextfield/scotiabanktakehome/MainActivity.kt
+++ app/src/main/java/com/jsontextfield/scotiabanktakehome/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jsontextfield.scotiabanktakehome.ui.theme.ScotiabankTakeHomeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScotiabankTakeHomeTheme {
+                var text by remember { mutableStateOf("") }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Row(
+                            modifier = Modifier.padding(10.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            OutlinedTextField(
+                                text,
+                                modifier = Modifier.weight(1f),
+                                label = { Text("Enter a GitHub user ID") },
+                                onValueChange = { text = it },
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            FilledTonalButton(onClick = {
+                                // Search GitHub using text
+                            }) {
+                                Text("Search")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScotiabankTakeHomeTheme {
-        Greeting("Android")
     }
 }
