1cc0872b472cd4fe344ef640eb191d3ab50c44b7
jason <tehblackboy@gmail.com>
2024-06-09 05:58:40 -0400

    implemented Parcelable in GitHubRepo and removed repos field from GitHubUser

--- app/src/main/java/com/jsontextfield/scotiabanktakehome/entities/GitHubRepo.kt
+++ app/src/main/java/com/jsontextfield/scotiabanktakehome/entities/GitHubRepo.kt
@@ -1,16 +1,52 @@
 package com.jsontextfield.scotiabanktakehome.entities
 
+import android.os.Parcel
+import android.os.Parcelable
 import org.json.JSONObject
 
 data class GitHubRepo(
     val name: String = "",
     val description: String = "",
-) {
-    companion object {
+    val lastUpdated: String = "",
+    val stars: Int = 0,
+    val forks: Int = 0,
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readInt(),
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeString(lastUpdated)
+        parcel.writeInt(stars)
+        parcel.writeInt(forks)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<GitHubRepo> {
+        override fun createFromParcel(parcel: Parcel): GitHubRepo {
+            return GitHubRepo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GitHubRepo?> {
+            return arrayOfNulls(size)
+        }
+
         fun fromJson(jsonObject: JSONObject): GitHubRepo {
             return GitHubRepo(
                 name = jsonObject.optString("name"),
                 description = jsonObject.optString("description"),
+                lastUpdated = jsonObject.optString("updated_at"),
+                stars = jsonObject.optInt("stargazers_count"),
+                forks = jsonObject.optInt("forks"),
             )
         }
     }
--- app/src/main/java/com/jsontextfield/scotiabanktakehome/entities/GitHubUser.kt
+++ app/src/main/java/com/jsontextfield/scotiabanktakehome/entities/GitHubUser.kt
@@ -5,7 +5,6 @@ import org.json.JSONObject
 data class GitHubUser(
     val name: String = "",
     val avatarUrl: String = "",
-    val repos: List<GitHubRepo> = emptyList()
 ) {
     companion object {
         fun fromJson(jsonObject: JSONObject): GitHubUser {
