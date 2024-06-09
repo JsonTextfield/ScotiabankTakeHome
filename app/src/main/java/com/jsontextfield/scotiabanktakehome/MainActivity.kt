4cc0ce8f2271a7a47b85e3437f35c3304fb62475
jason <tehblackboy@gmail.com>
2024-06-09 06:06:04 -0400

    updated MainActivity to use the components that were moved into separate files

--- app/src/main/java/com/jsontextfield/scotiabanktakehome/MainActivity.kt
+++ app/src/main/java/com/jsontextfield/scotiabanktakehome/MainActivity.kt
@@ -1,55 +1,77 @@
 package com.jsontextfield.scotiabanktakehome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import com.jsontextfield.scotiabanktakehome.ui.components.RepoList
+import com.jsontextfield.scotiabanktakehome.ui.components.SearchBar
+import com.jsontextfield.scotiabanktakehome.ui.components.UserInfo
 import com.jsontextfield.scotiabanktakehome.ui.theme.ScotiabankTakeHomeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModelFactory = MainViewModelFactory()
+        val mainViewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
+
         enableEdgeToEdge()
         setContent {
             ScotiabankTakeHomeTheme {
-                var text by remember { mutableStateOf("") }
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        Row(
-                            modifier = Modifier.padding(10.dp),
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            OutlinedTextField(
-                                text,
-                                modifier = Modifier.weight(1f),
-                                label = { Text("Enter a GitHub user ID") },
-                                onValueChange = { text = it },
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        Surface(shadowElevation = 10.dp) {
+                            TopAppBar(title = { Text(stringResource(R.string.app_name)) })
+                        }
+                    },
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        val context = LocalContext.current
+                        val searchText by mainViewModel.searchText.collectAsState()
+                        SearchBar(
+                            searchText,
+                            onTextChanged = { mainViewModel.onSearchTextChanged(it) },
+                            onSearchButtonPressed = {
+                                mainViewModel.getUserData(context)
+                                mainViewModel.getUserRepos(context)
+                            },
+                        )
+
+                        val user by mainViewModel.user.collectAsState()
+                        user?.let {
+                            UserInfo(it)
+                        }
+
+                        val repos by mainViewModel.repos.collectAsState()
+                        RepoList(repos) {
+                            startActivity(
+                                Intent(this@MainActivity, RepoDetailsActivity::class.java).apply {
+                                    putExtra("repo", it)
+                                }
                             )
-                            Spacer(modifier = Modifier.width(10.dp))
-                            FilledTonalButton(onClick = {
-                                // Search GitHub using text
-                            }) {
-                                Text("Search")
-                            }
                         }
                     }
                 }
