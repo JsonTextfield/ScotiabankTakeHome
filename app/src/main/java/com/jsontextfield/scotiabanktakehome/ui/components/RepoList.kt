538eff92ffc4d733523d08cf041c9f6f95b16fcd
jason <tehblackboy@gmail.com>
2024-06-09 20:59:56 -0400

    moved AnimatedVisibility out of RepoList and UserInfo

--- app/src/main/java/com/jsontextfield/scotiabanktakehome/ui/components/RepoList.kt
+++ app/src/main/java/com/jsontextfield/scotiabanktakehome/ui/components/RepoList.kt
@@ -1,11 +1,5 @@
 package com.jsontextfield.scotiabanktakehome.ui.components
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -24,28 +18,21 @@ fun RepoList(
     repos: List<GitHubRepo> = emptyList(),
     onItemClick: (GitHubRepo) -> Unit = {},
 ) {
-    AnimatedVisibility(
-        visible = repos.isNotEmpty(),
-        enter = fadeIn(animationSpec = tween(500)) + slideInVertically(animationSpec = tween(1000)),
-        exit = fadeOut(animationSpec = tween(500)) + slideOutVertically(animationSpec = tween(1000)),
-    ) {
-        LazyColumn {
-            items(repos) { repo ->
-                Card(
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .clickable { onItemClick(repo) },
-                    elevation = CardDefaults.cardElevation(10.dp)
-                ) {
-                    ListItem(
-                        headlineContent = { Text(repo.name) },
-                        supportingContent = {
-                            if (repo.description != "null" && repo.description.isNotEmpty()) {
-                                Text(repo.description)
-                            }
-                        },
-                    )
-                }
+    LazyColumn {
+        items(repos) { repo ->
+            Card(
+                modifier = Modifier.padding(10.dp),
+                elevation = CardDefaults.cardElevation(10.dp),
+            ) {
+                ListItem(
+                    modifier = Modifier.clickable { onItemClick(repo) },
+                    headlineContent = { Text(repo.name) },
+                    supportingContent = {
+                        if (repo.description.isNotEmpty()) {
+                            Text(repo.description)
+                        }
+                    },
+                )
             }
         }
     }
--- app/src/main/java/com/jsontextfield/scotiabanktakehome/ui/components/UserInfo.kt
+++ app/src/main/java/com/jsontextfield/scotiabanktakehome/ui/components/UserInfo.kt
@@ -20,22 +20,16 @@ import com.jsontextfield.scotiabanktakehome.entities.GitHubUser
 
 @Composable
 fun UserInfo(userData: GitHubUser? = null) {
-    AnimatedVisibility(
-        enter = fadeIn(animationSpec = tween(500)) + slideInVertically(animationSpec = tween(1000)),
-        exit = fadeOut(animationSpec = tween(500)) + slideOutVertically(animationSpec = tween(1000)),
-        visible = userData != null
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            AsyncImage(
-                userData?.avatarUrl,
-                contentDescription = "avatar",
-                modifier = Modifier.widthIn(max = 100.dp),
-                contentScale = ContentScale.Inside
-            )
-            Text(
-                userData?.name ?: "",
-                modifier = Modifier.padding(10.dp),
-            )
-        }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        AsyncImage(
+            userData?.avatarUrl,
+            contentDescription = "avatar",
+            modifier = Modifier.widthIn(max = 100.dp),
+            contentScale = ContentScale.Inside
+        )
+        Text(
+            userData?.name ?: "",
+            modifier = Modifier.padding(10.dp),
+        )
     }
 }
